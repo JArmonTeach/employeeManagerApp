6f2f8461ca2b7e059e50b6b68666241e0c4a60b9
Armon Antolin <74826437+JArmonTeach@users.noreply.github.com>
2022-11-10 14:14:42 -0500

    feat: getters and setters add for employee;toString add to employee class

--- src/main/java/com/getarrays/employeemanager/model/Employee.java
+++ src/main/java/com/getarrays/employeemanager/model/Employee.java
@@ -16,14 +16,13 @@ public class Employee implements Serializable {
     private String department;
     private String jobTitle;
     private String location;
-    private Boolean remoteStatus;
     private String imageUrl;
     @Column(nullable = false, updatable = false)
     private String employeeCode;
 
     public Employee(){}
 
-    public Employee(String firstName, String lastName, String email, String phone, String department, String jobTitle, String location, Boolean remoteStatus, String imageUrl, String employeeCode){
+    public Employee(String firstName, String lastName, String email, String phone, String department, String jobTitle, String location, String imageUrl, String employeeCode){
         this.firstName = firstName;
         this.lastName = lastName;
         this.email= email;
@@ -31,11 +30,104 @@ public class Employee implements Serializable {
         this.department = department;
         this.jobTitle = jobTitle;
         this.location = location;
-        this.remoteStatus = remoteStatus;
         this.imageUrl = imageUrl;
         this.employeeCode = employeeCode;
     }//end constructor
 
+    public Long getId(){
+        return id;
+    }
+
+    public void setId(Long id){
+        this.id = id;
+    }
+
+    public String getFirstName(){
+        return firstName;
+    }
+
+    public void setId(String firstName){
+        this.firstName = firstName;
+    }
+
+    public String getLastName(){
+        return lastName;
+    }
+
+    public void setLastName(String lastName){
+        this.lastName = lastName;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    public String getPhone(){
+        return phone;
+    }
+
+    public void setPhone(String phone){
+        this.phone = phone;
+    }
+
+    public String getDepartment(){
+        return department;
+    }
+
+    public void setDepartment(String department){
+        this.department = department;
+    }
+
+    public String getJobTitle(){
+        return jobTitle;
+    }
+
+    public void setJobTitle(String jobTitle){
+        this.jobTitle = jobTitle;
+    }
+
+    public String getLocation(){
+        return location;
+    }
+
+    public void setLocation(String location){
+        this.location = location;
+    }
+
+    public String getImageUrl(){
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl){
+        this.imageUrl = imageUrl;
+    }
+
+    public String getEmployeeCode(){
+        return employeeCode;
+    }
+
+    public void setEmployeeCode(String employeeCode){
+        this.employeeCode = employeeCode;
+    }
+
+    @Override
+    public String toString(){
+        return "Employee{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                ", department='" + department + '\'' +
+                ", jobTitle='" + jobTitle + '\'' +
+                ", location='" + location + '\'' +
+                ", imageUrl='" + imageUrl + '\'' +
+                '}';
+    }//end toString
 }//end class
 
 
