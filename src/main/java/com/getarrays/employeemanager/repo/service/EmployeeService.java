cc5a4641114e5eeb9d0f133fd051d7e3c616ad67
Armon Antolin <74826437+JArmonTeach@users.noreply.github.com>
2022-11-12 21:01:45 -0500

    feat: exception handling for employeeService add

--- src/main/java/com/getarrays/employeemanager/repo/service/EmployeeService.java
+++ src/main/java/com/getarrays/employeemanager/repo/service/EmployeeService.java
@@ -1,8 +1,13 @@
 package com.getarrays.employeemanager.repo.service;
 
+import com.getarrays.employeemanager.model.Employee;
 import com.getarrays.employeemanager.repo.EmployeeRepo;
+import com.getarrays.employeemanager.repo.exception.UserNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
 
 @Service
 public class EmployeeService {
@@ -11,5 +16,26 @@ public class EmployeeService {
     @Autowired
     public EmployeeService(EmployeeRepo employeeRepo){
         this.employeeRepo = employeeRepo;
+    }
+
+    public Employee addEmployee(Employee employee){
+        employee.setEmployeeCode(UUID.randomUUID().toString());
+        return employeeRepo.save(employee);
+    }
+
+    public List<Employee> findAllEmployees(){
+        return employeeRepo.findAll();
+    }
+
+    public Employee updateEmployee(Employee employee){
+        return employeeRepo.save(employee);
+    }
+
+    public Employee findEmployeeById(Long id){
+        return employeeRepo.findEmployeeById(id).orElseThrow(() -> new UserNotFoundException("User by id " + id + " was not found."));
+    }
+
+    public void deleteEmployee(Long id){
+        employeeRepo.deleteEmployeeById(id);
     }
 }
