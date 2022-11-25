abd8e794d2b30b273676606903e486bb00baf016
Armon Antolin <74826437+JArmonTeach@users.noreply.github.com>
2022-11-25 18:09:01 -0500

    debug: add transational import to service

--- src/main/java/com/getarrays/employeemanager/repo/service/EmployeeService.java
+++ src/main/java/com/getarrays/employeemanager/repo/service/EmployeeService.java
@@ -5,8 +5,8 @@ import com.getarrays.employeemanager.repo.EmployeeRepo;
 import com.getarrays.employeemanager.repo.exception.UserNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
 import java.util.List;
 import java.util.UUID;
 
